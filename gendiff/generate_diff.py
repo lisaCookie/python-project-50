312929c91b2f50f9f9968136aae245db21ba46aa
lisa <lisavetabelova@gmail.com>
2025-06-11 17:36:10 +0300

    modified:   gendiff/formatters/stylish.py         modified:   gendiff/generate_diff.py

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,5 +1,4 @@
 import json
-
 from gendiff.formatters.json import format_json
 from gendiff.formatters.plain import format_plain
 from gendiff.formatters.stylish import stylish
@@ -15,23 +14,27 @@ def load_file(filepath):
         with open(filepath, 'r') as f:
             return json.load(f)
     else:
-        raise ValueError("Unsupported file format")
+        raise ValueError(f"Unsupported file format: {filepath}")
 
 
 def compute_diff(data1, data2):
     diff = []
-    keys = set(data1.keys()) | set(data2.keys())
-    for key in sorted(keys):
-        if key in data1 and key not in data2:
-            diff.append(
-                {'key': key, 'type': 'added', 'new_value': data1[key]} 
-            )
-        elif key not in data1 and key in data2:
-            diff.append(
-                {'key': key, 'type': 'removed', 'old_value': data2[key]}  
-            )
-        elif (isinstance(data1[key], dict) and
-              isinstance(data2[key], dict)):
+    keys = sorted(set(data1.keys()) | set(data2.keys()))
+    
+    for key in keys:
+        if key not in data2:
+            diff.append({
+                'key': key,
+                'type': 'removed',
+                'old_value': data1[key]
+            })
+        elif key not in data1:
+            diff.append({
+                'key': key,
+                'type': 'added',
+                'new_value': data2[key]
+            })
+        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
             diff.append({
                 'key': key,
                 'type': 'nested',
@@ -41,24 +44,29 @@ def compute_diff(data1, data2):
             diff.append({
                 'key': key,
                 'type': 'modified',
-                'old_value': data2[key],  
-                'new_value': data1[key]  
+                'old_value': data1[key],
+                'new_value': data2[key]
             })
         else:
-            diff.append({'key': key, 'type': 'unchanged', 'value': data1[key]})
+            diff.append({
+                'key': key,
+                'type': 'unchanged',
+                'value': data1[key]
+            })
+    
     return diff
 
 
-def generate_diff(filepath1, filepath2, format_name='stylish') -> str:
+def generate_diff(filepath1, filepath2, format_name='stylish'):
     data1 = load_file(filepath1)
     data2 = load_file(filepath2)
     diff = compute_diff(data1, data2)
 
     if format_name == 'stylish':
-        return "{\n" + stylish(diff) + "\n}"
+        return stylish(diff)
     elif format_name == 'plain':
         return format_plain(diff)
     elif format_name == 'json':
         return format_json(diff)
     else:
-        raise ValueError("Unsupported format name")
+        raise ValueError(f"Unknown format: {format_name}")
